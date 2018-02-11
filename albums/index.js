8d19a16e0ecf6960f9a2a4f5302a4056fafdf67d
Max Magee <maxmagee@users.noreply.github.com>
2018-02-10 18:07:20 -0600

    First custom component.

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,13 @@
+// Import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+// Create a component
+const App = () => (
+    <Header />
+);
+
+// Render it to the device
+// The first parameter should match the name of the application we created
 AppRegistry.registerComponent('albums', () => App);
