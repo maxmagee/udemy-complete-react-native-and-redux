e4c0b52f1875ce132afa7e7c426fee78b8abc4c0
Max Magee <maxmagee@users.noreply.github.com>
2018-02-17 13:09:05 -0600

    Added 'axios' package to handle retrieving data from our external api.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,8 +1,19 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List!!!</Text>
