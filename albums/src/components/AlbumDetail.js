8c2c36e008127023e87b818a391b48cc4beec616
Max Magee <maxmagee@users.noreply.github.com>
2018-02-17 14:13:44 -0600

    Rendering a styled list of albums.

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+    const { artist, image, thumbnail_image, title } = album;
+    const { 
+        headerContentStyle,
+        headerTextStyle,
+        imageStyle,
+        thumbnailContainerStyle,
+        thumbnailStyle
+    } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle} 
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
+};
+
+export default AlbumDetail;
--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = {
@@ -12,11 +13,16 @@ class AlbumList extends Component {
             .then(response => this.setState({ albums: response.data }));
     }
 
+    renderAlbums() {
+        return this.state.albums.map((album) => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
-        console.log(this.state);
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
