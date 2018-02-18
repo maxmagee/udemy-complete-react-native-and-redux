38d9370c46e39e83e0fd9dc383d50f9e38566716
Max Magee <maxmagee@users.noreply.github.com>
2018-02-18 14:21:24 -0600

    Copied over reusable components from albumbs project.

---
+++ auth/src/components/common/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => (
+    <View style={styles.containerStyle}>
+        {props.children}
+    </View>
+);
+
+const styles = {
+    containerStyle: {
+        backgroundColor: '#fff',
+        borderBottomWidth: 0,
+        borderColor: '#ddd',
+        borderRadius: 2,
+        borderWidth: 1,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2
+    }
+};
+
+export default Card;
