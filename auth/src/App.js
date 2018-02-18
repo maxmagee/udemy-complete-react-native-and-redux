1a744b64a8e89d8b14c77fd868e53342c91d50cb
Max Magee <maxmagee@users.noreply.github.com>
2018-02-18 16:03:56 -0600

    Initialization of Firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAUD9wsehHGlxvA63QixppxHVl-oRXywW8',
+            authDomain: 'auth-f6cd2.firebaseapp.com',
+            databaseURL: 'https://auth-f6cd2.firebaseio.com',
+            projectId: 'auth-f6cd2',
+            storageBucket: 'auth-f6cd2.appspot.com',
+            messagingSenderId: '1087940718004'
+          });
+    }
     render() {
         return (
             <View>
