a981bbed4fce2096ff2da7ce66e152e31b87682a
Max Magee <maxmagee@users.noreply.github.com>
2018-02-22 20:06:01 -0600

    Tapping on a list item will expand a description. Tech Stack app finished.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+    LayoutAnimation,    
+    Text,
+    TouchableWithoutFeedback, 
+    View 
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.easeInEaseOut();
+    }
+    renderDescription() {
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={{ flex: 1 }}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            );
+        }
+    }
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -23,4 +54,10 @@ const styles = {
     }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
